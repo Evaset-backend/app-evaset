93b7e9e628ab286832f30754e48f6753747ac87f
Anvarprimov08 <anvarprimov07@gmail.com>
2023-02-20 12:35:01 +0500

    BusinessService TODO COMMIT IS ADDED FOR USER

--- src/main/java/uz/pdp/springsecurity/service/BusinessService.java
+++ src/main/java/uz/pdp/springsecurity/service/BusinessService.java
@@ -27,6 +27,9 @@ public class BusinessService {
     @Autowired
     TariffRepository tariffRepository;
 
+    @Autowired
+    UserService userService;
+
 
     public ApiResponse add(BusinessDto businessDto) {
         if (businessRepository.existsByName(businessDto.getName()))
@@ -45,6 +48,10 @@ public class BusinessService {
                 business,
                 true
         ));
+        /**
+         * TODO ADD USER AFTER BUSINESS
+         */
+//        userService.add()
         return new ApiResponse("ADDED", true);
     }
 
