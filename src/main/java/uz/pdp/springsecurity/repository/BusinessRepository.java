53d2ac3d84449cd793e78a01aa119dc4f8417525
Yoqub <yyboboraximov@gmail.com>
2023-02-21 00:46:58 +0500

    commit_16

--- src/main/java/uz/pdp/springsecurity/repository/BusinessRepository.java
+++ src/main/java/uz/pdp/springsecurity/repository/BusinessRepository.java
@@ -3,9 +3,16 @@ package uz.pdp.springsecurity.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.pdp.springsecurity.entity.Business;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BusinessRepository extends JpaRepository<Business, UUID> {
 
     boolean existsByName(String name);
+
+
+    Optional<Business> findByName(String name);
+
+    List<Business> findAllByDeleteIsFalse();
 }
