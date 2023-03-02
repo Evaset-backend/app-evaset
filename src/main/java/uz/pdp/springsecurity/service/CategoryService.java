de32680e0fe33e3428bd82b596bf8f194f6c0d14
samir <sharipovsamir480@gmail.com>
2023-03-02 17:43:49 +0500

    02.03.2023 12

--- src/main/java/uz/pdp/springsecurity/service/CategoryService.java
+++ src/main/java/uz/pdp/springsecurity/service/CategoryService.java
@@ -25,7 +25,7 @@ public class CategoryService {
     public ApiResponse add(CategoryDto categoryDto) {
         UUID businessId = categoryDto.getBusinessId();
         Optional<Business> optionalBusiness = businessRepository.findById(businessId);
-        if (optionalBusiness.isEmpty()){
+        if (optionalBusiness.isEmpty()) {
             return new ApiResponse("Business Not Found");
         }
 
@@ -45,26 +45,31 @@ public class CategoryService {
         }
         categoryRepository.save(category);
 
-        return new ApiResponse("Added",true,category);
+        return new ApiResponse("Added", true, category);
 
     }
 
+
     public ApiResponse edit(UUID id, CategoryDto categoryDto) {
-        if (!categoryRepository.existsById(id)) return new ApiResponse("NOT FOUND", false);
-        Category reCategory = new Category();
-        Optional<Category> optionalCategory = categoryRepository.findById(categoryDto.getParentCategory());
-        if (optionalCategory.isPresent()) {
-            reCategory = optionalCategory.get();
+
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isEmpty()) {
+            return new ApiResponse("not found", false);
         }
 
-        Category category = categoryRepository.getById(id);
+        Category category = optionalCategory.get();
         category.setName(categoryDto.getName());
         category.setDescription(categoryDto.getDescription());
-        category.setParentCategory(reCategory);
+
+        if (categoryDto.getParentCategory() != null) {
+            Optional<Category> optional = categoryRepository.findById(id);
+            optional.ifPresent(category::setParentCategory);
+        }
 
         categoryRepository.save(category);
         return new ApiResponse("EDITED", true);
     }
+
 
     public ApiResponse get(UUID id) {
         if (!categoryRepository.existsById(id)) return new ApiResponse("NOT FOUND", false);
@@ -82,9 +87,9 @@ public class CategoryService {
 
     public ApiResponse getAllByBusinessId(UUID businessId) {
 //        List<Category> allByBusinessId = categoryRepository.findByBusinessIdAndParentCategoryNull(businessId);
-        List<Category> allByBusinessId  = categoryRepository.findAllByBusiness_Id(businessId);
-        if (allByBusinessId.isEmpty()) return new ApiResponse("NOT FOUND",false);
-        return new ApiResponse("FOUND",true,allByBusinessId);
+        List<Category> allByBusinessId = categoryRepository.findAllByBusiness_Id(businessId);
+        if (allByBusinessId.isEmpty()) return new ApiResponse("NOT FOUND", false);
+        return new ApiResponse("FOUND", true, allByBusinessId);
     }
 
     public ApiResponse addChildCategory(CategoryDto categoryDto) {
@@ -104,28 +109,28 @@ public class CategoryService {
         );
         category = categoryRepository.save(category);
         CategoryDto dto = generateCategoryDtoFromCategory(category);
-        return new ApiResponse("ADDED", true, dto );
+        return new ApiResponse("ADDED", true, dto);
     }
 
     public ApiResponse getAllChildCategoryById(UUID id) {
         List<Category> allByBusiness_id = categoryRepository.findAllByBusinessIdAndParentCategoryNotNull(id);
-        if (allByBusiness_id.isEmpty()){
+        if (allByBusiness_id.isEmpty()) {
             return new ApiResponse("CATEGORIES NOT FOUND", false);
         }
         List<CategoryDto> dtos = new ArrayList<>();
         for (Category category : allByBusiness_id) {
             dtos.add(generateCategoryDtoFromCategory(category));
         }
-        return new ApiResponse("All Child Category", true,  dtos);
+        return new ApiResponse("All Child Category", true, dtos);
     }
 
-    public CategoryDto generateCategoryDtoFromCategory(Category category){
+    public CategoryDto generateCategoryDtoFromCategory(Category category) {
         CategoryDto dto = new CategoryDto();
         dto.setId(category.getId());
         dto.setName(category.getName());
         dto.setDescription(category.getDescription());
         dto.setBusinessId(category.getBusiness().getId());
-        if (category.getParentCategory().getId()!=null) {
+        if (category.getParentCategory().getId() != null) {
             dto.setParentCategory(category.getParentCategory().getId());
         }
         dto.setParentCategory(null);
@@ -134,23 +139,23 @@ public class CategoryService {
 
     public ApiResponse getAllChildcategoryByParentId(UUID id) {
         List<Category> categoriesByParentCategoryId = categoryRepository.findCategoriesByParentCategoryId(id);
-        if (categoriesByParentCategoryId.isEmpty()){
-            return new ApiResponse(false,"Not yet child category");
+        if (categoriesByParentCategoryId.isEmpty()) {
+            return new ApiResponse(false, "Not yet child category");
         }
         List<CategoryDto> dtos = new ArrayList<>();
         for (Category category : categoriesByParentCategoryId) {
             dtos.add(generateCategoryDtoFromCategory(category));
         }
-        return new ApiResponse("All child category",true, dtos);
+        return new ApiResponse("All child category", true, dtos);
     }
 
     public ApiResponse getAllParentCategory(UUID id) {
 
         List<Category> categoriesByParentCategoryId = categoryRepository.findAllByBusiness_IdAndAndParentCategoryNull(id);
-        if (categoriesByParentCategoryId.isEmpty()){
-            return new ApiResponse(false,"Not Found");
+        if (categoriesByParentCategoryId.isEmpty()) {
+            return new ApiResponse(false, "Not Found");
         }
         List<Category> categoryList = categoriesByParentCategoryId;
-        return new ApiResponse("All Parent Categories ",true, categoryList);
+        return new ApiResponse("All Parent Categories ", true, categoryList);
     }
 }
