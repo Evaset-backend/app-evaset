06061f62ac63f9bb9aad81e6610cb05cf929ac53
samir <sharipovsamir480@gmail.com>
2023-03-06 20:02:32 +0500

    06.03.2023 5

--- src/main/java/uz/pdp/springsecurity/repository/ProductTypePriceRepository.java
+++ src/main/java/uz/pdp/springsecurity/repository/ProductTypePriceRepository.java
@@ -20,11 +20,11 @@ public interface ProductTypePriceRepository extends JpaRepository<ProductTypePri
     List<ProductTypePrice> findAllByProduct_Brand_IdAndProduct_Branch_IdAndProduct_ActiveIsTrue(UUID product_brand_id, UUID product_branch_id);
 
 
-    List<ProductTypePrice> findAllByProduct_BrandIdAndProduct_BranchIdAndProduct_Active(UUID product_brand_id, UUID product_branch_id);
-
-    List<ProductTypePrice> findAllByProduct_BrandIdAndProduct_CategoryIdAndProduct_BranchId(UUID product_brand_id, UUID product_category_id, UUID product_branch_id);
-    List<ProductTypePrice> findAllByProduct_CategoryIdAndProduct_BranchId(UUID product_category_id, UUID product_branch_id);
-    Optional<ProductTypePrice> findByProductId(UUID product_id);
+//    List<ProductTypePrice> findAllByProduct_BrandIdAndProduct_BranchIdAndProduct_Active(UUID product_brand_id, UUID product_branch_id);
+//
+//    List<ProductTypePrice> findAllByProduct_BrandIdAndProduct_CategoryIdAndProduct_BranchId(UUID product_brand_id, UUID product_category_id, UUID product_branch_id);
+//    List<ProductTypePrice> findAllByProduct_CategoryIdAndProduct_BranchId(UUID product_category_id, UUID product_branch_id);
+//    Optional<ProductTypePrice> findByProductId(UUID product_id);
 
     boolean existsByBarcodeAndProduct_BusinessId(String barcode, UUID businessId);
     boolean existsByBarcodeAndProduct_BusinessIdAndIdIsNot(String barcode, UUID businessId, UUID productTypePriceId);
