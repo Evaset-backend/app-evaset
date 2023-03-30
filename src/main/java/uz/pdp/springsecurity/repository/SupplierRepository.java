40937ef11a78c5e7f982c840c7b1e9c1e262121d
Anvarprimov08 <anvarprimov07@gmail.com>
2023-03-30 16:09:09 +0500

    30.3.2023 commit- 3 suppliyer delete

--- src/main/java/uz/pdp/springsecurity/repository/SupplierRepository.java
+++ src/main/java/uz/pdp/springsecurity/repository/SupplierRepository.java
@@ -1,7 +1,6 @@
 package uz.pdp.springsecurity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import uz.pdp.springsecurity.entity.Supplier;
 
 import java.util.List;
